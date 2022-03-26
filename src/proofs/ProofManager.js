ed2080144d4fc7abdb85ecdc342477626248a8f1
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2022-03-26 11:05:28 +0100

    Add team info to proof

--- src/proofs/ProofManager.js
+++ src/proofs/ProofManager.js
@@ -11,15 +11,19 @@ class ProofManager {
 		this.collection = database.collection("proofs");
 	}
 
-	async create({keys}) {
+	async create({keys, team}) {
 		const id = nanoid(16);
 		const scores = {};
 		keys.forEach(key => scores[key] = 0);
-		const proof = new ProofModel({id, scores, images: []});
+		const proof = new ProofModel({id, scores, images: [], team});
 
 		await this.collection.insertOne(proof.toDocument());
 
 		return proof;
+	}
+
+	async getTeam(id) {
+		return (await this.get({id})).team;
 	}
 
 	async addImage(id, image) {
@@ -32,11 +36,27 @@ class ProofManager {
 		await this.collection.replaceOne({id}, proof);
 	}
 
+	async setScores(id, scores) {
+		const proof = await this.getModel({id});
+
+		if(proof === undefined) return false;
+
+		for(const key of Object.keys(proof.scores)) {
+			if(typeof scores[key] !== "number") return false;
+
+			proof.scores[key] = scores[key];
+		}
+
+		await this.collection.replaceOne({id}, proof.toDocument());
+
+		return true;
+	}
+
 	get(data) {
 		return this.collection.findOne(data);
 	}
 
-	getModel(data) {
+	async getModel(data) {
 		return new ProofModel(await this.get(data));
 	}
 }
--- src/proofs/ProofModel.js
+++ src/proofs/ProofModel.js
@@ -1,15 +1,17 @@
 class ProofModel {
-	constructor({id, scores, images}) {
+	constructor({id, scores, images, team}) {
 		this.id = id;
 		this.scores = scores;
 		this.images = images;
+		this.team = team;
 	}
 
 	toDocument() {
 		return {
 			id: this.id,
 			scores: this.scores,
-			images: this.images
+			images: this.images,
+			team: this.team
 		};
 	}
 }
