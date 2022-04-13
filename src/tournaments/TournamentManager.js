2443b4e2857d97421e4a4f2fa79e9978c32657d5
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2022-04-13 23:42:39 +0200

    Fix finding local tournaments

--- src/tournaments/TournamentManager.js
+++ src/tournaments/TournamentManager.js
@@ -150,7 +150,7 @@ class TournamentManager {
     }
 
     async getClosest(location, radius) {
-        const tournamentsData = await (await this.collection.aggregate([{$geoNear: {near: {type: "Point", coordinates: location}, maxDistance: radius}}])).toArray();
+        const tournamentsData = await (await this.collection.aggregate([{$geoNear: {near: {type: "Point", coordinates: location}, maxDistance: radius, distanceField: "dist.calculated"}}])).toArray();
 
         return tournamentsData.map(tournament => new TournamentModel(tournament));
     }
